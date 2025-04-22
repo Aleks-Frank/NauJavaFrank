03131631fe3275c3e30afe5e624b19bc316288d4
Alexandr-Frank <SACHAFRANK2004@yandex.ru>
2025-04-22 16:31:49 +0500

    True homework#4

--- Library/Library/src/main/java/com/example/Library/repository/UserRepositoryDB.java
+++
@@ -1,8 +0,0 @@
-package com.example.Library.repository;
-
-import com.example.Library.entity.UserEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepositoryDB extends CrudRepository<UserEntity, Long> {
-    UserEntity findByUsername(String username);
-}
